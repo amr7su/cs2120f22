f9e0395e725f9cbbdac121be86fe231b08793d3d
amr7su <amr7su@virginia.edu>
2022-10-19 23:07:16 +0000

    hw4

--- src/homeworks/hw4/hw4.lean
+++ src/homeworks/hw4/hw4.lean
@@ -13,7 +13,8 @@ answer.
 -/
 
 def and_associative : Prop := 
-  _
+  ∀ (P Q R : Prop),
+    P ∧ (Q ∧ R) ↔ (P ∧ Q) ∧ R
 
 
 /- #1B [10 points]
@@ -24,7 +25,14 @@ that you use in your reasoning.
 -/
 
 /-
-Answer: 
+Answer: According to the intro rule for iff, it suffices to prove both
+directions of the proposition separately. Left to right, we have that
+P AND (Q AND R) implies (P AND Q) AND R. Applying and elimination twice to (Q AND R)
+gives us, separately, P, Q, and R. Then we apply and introduction to P and Q, and then
+again to (P AND Q) and R to yield (P AND Q) AND R.
+The same logic is used in proving the right to left proposition,
+applying and elimination twice to (P AND Q) and then and introduction to Q and R
+and then to P and (Q AND R) to yield P AND (Q AND R).
 -/
 
 /- #1C [5 points]
@@ -46,14 +54,27 @@ analogous to the proposition about ∧ in #1.
 -/
 
 def or_associative : Prop := 
-  _
-
+  ∀ (P Q R : Prop),
+    P ∨ (Q ∨ R) ↔ (P ∨ Q) ∨ R
 
 /- #2B [10 points]
 
 Write an English language proof of it, citing
 the specific inference rules you use in your
 reasoning.
+
+According to the intro rule for iff, it suffices to prove both
+directions of the proposition separately. Left to right, we have that
+P OR (Q OR R) implies (P OR Q) OR R. In the case that P is true, then (P OR Q) must
+be true by or introduction, and thus (P OR Q) OR R must be true by introduction again.
+In the case that (Q OR R) is true, then at least one of Q or R is true. In the case that
+Q is true, then (P OR Q) is true by or introduction, and (P OR Q) OR R is also true by or introduction.
+In the case that R is true, (P OR Q) OR R is true by or introduction. In the case that
+P Q and R are false, the proposition need not be proven because false implies true.
+Thus the proposition holds in all cases and is true.
+The same logic is used in proving the right to left proposition,
+checking each case and applying or introduciton to the appropriate members to
+contstruct the desired expression.
 -/
 
 
@@ -72,7 +93,8 @@ Write a formal statement of the proposition.
 -/
 
 def arrow_transitive : Prop :=
-  _
+  ∀ (P Q R : Prop),
+    (P → Q) → (Q → R) → (P → R)
 
 
 /- #3B [10 points]
@@ -86,12 +108,18 @@ have a proof of X, you can derive a proof of Y by
 arrow elimination. Think of it as applying a proof
 of an implication to a proof of its premise to get
 yourself a proof of its conclusion.
+
+Answer: Assume that X is true. Then, by arrow elimination on X → Y,
+Y must be true. Then arrow elimination on (Y → Z) gives us that Z is true.
 -/
 
 
 /- #3C [5 points]. 
 Write a formal proof of it.
 -/
+theorem arrow_trans_true : arrow_transitive :=
+begin
+end
 
 
 /- #4
@@ -108,7 +136,7 @@ logic by completing the following answer.
 
 def contrapositive : Prop :=
   ∀ (Raining Wet : Prop), 
-    (Raining → Wet) → (¬Raining → ¬Wet)
+    (Raining → Wet) → (¬Wet → ¬Raining)
 
 
 /- #4B [10 points]. 
@@ -116,10 +144,19 @@ def contrapositive : Prop :=
 
 theorem contrapositive_valid : contrapositive :=
 begin
+assume h1: Raining,
+assume h : Raining → Wet,
+
+end
 
 /- #4C [5 points]. 
 
 Give an English language proof of it.
+
+If raining implies that it is wet, assume that it is raining and it is not wet. Now apply
+arrow elimination to (Raining implies Wet) to derive a proof that it is wet. Now we have
+that it is wet and it is not wet, and absurdity. Thus it is not possible that it is
+raining when it is not wet.
 -/
 
 
@@ -139,9 +176,9 @@ theorem demorgan1 :
       ¬(X ∨ Y) → (¬X ∧ ¬Y) :=
 begin
 assume em X Y nxory,
-cases (em X) with x nx,
+cases (em X) with x nx, 
 let foo := or.intro_left Y x,
-_
+
 end
 
 /-
